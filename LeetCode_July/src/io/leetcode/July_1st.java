593792cfec0185643dddc234526585fa0c724527
Apocalypse <harshkulkarni45@gmail.com>
2020-07-03 13:57:11 +0530

    Number of rows for arranging coins.

--- LeetCode_July/src/io/leetcode/July_1st.java
+++ LeetCode_July/src/io/leetcode/July_1st.java
@@ -1,10 +1,26 @@
 package io.leetcode;
 
+
 public class July_1st {
 
 	public static void main(String[] args) {
 		
-
+	int n = 10;
+	int result = arrangeCoins(n);	
+	
+	System.out.println(result);
+	
 	}
 
+	public static int arrangeCoins(int n) {
+        
+		long k = (long) Math.sqrt(2*(long)n);
+		
+		long sum = (k*(k+1))/2;
+		
+		if(sum>n) return (int)(k-1);
+		
+		return (int)k;
+        
+    }
 }
