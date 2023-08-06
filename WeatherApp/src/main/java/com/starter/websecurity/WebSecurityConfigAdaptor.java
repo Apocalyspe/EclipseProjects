7a06150eb3b4d6920deedbb0dde5dfbe735bbab6
macos <macos@macoss-air.station>
2023-08-06 13:12:38 +0100

    Committing WeatherApp starter project

---
+++ WeatherApp/src/main/java/com/starter/websecurity/WebSecurityConfigAdaptor.java
@@ -0,0 +1,13 @@
+package com.starter.websecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
+
+@EnableWebSecurity
+public class WebSecurityConfigAdaptor {
+	
+	
+	
+}
